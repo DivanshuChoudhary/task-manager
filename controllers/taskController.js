4c1f69d64f2574d55ed1454a861762dd711cef3f
DivanshuChoudhary <choudharydivanshu75@gmail.com>
2026-07-03 14:04:03 +0530

    add delete button

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -3,41 +3,65 @@ const path = require("path");
 
 const filePath = path.join(__dirname, "../data/tasks.json");
 
-// Read Tasks
+// Read File
+function readTasks() {
+    const data = fs.readFileSync(filePath);
+    return JSON.parse(data);
+}
+
+// Write File
+function writeTasks(tasks) {
+    fs.writeFileSync(filePath, JSON.stringify(tasks, null, 2));
+}
+
+// GET Tasks
 const getTasks = (req, res) => {
 
-    const data = fs.readFileSync(filePath);
-
-    const tasks = JSON.parse(data);
+    const tasks = readTasks();
 
     res.json(tasks);
 
 };
 
-// Add Task
+// POST Task
 const addTask = (req, res) => {
 
-    const { text } = req.body;
-
-    const data = fs.readFileSync(filePath);
-
-    const tasks = JSON.parse(data);
+    const tasks = readTasks();
 
     const newTask = {
         id: Date.now(),
-        text,
+        text: req.body.text,
         completed: false
     };
 
     tasks.push(newTask);
 
-    fs.writeFileSync(filePath, JSON.stringify(tasks, null, 2));
+    writeTasks(tasks);
 
     res.status(201).json(newTask);
 
 };
 
+// DELETE Task
+
+const deleteTask = (req, res) => {
+
+    const id = Number(req.params.id);
+
+    let tasks = readTasks();
+
+    tasks = tasks.filter(task => task.id !== id);
+
+    writeTasks(tasks);
+
+    res.json({
+        message: "Task Deleted"
+    });
+
+};
+
 module.exports = {
     getTasks,
-    addTask
+    addTask,
+    deleteTask
 };
--- index.js
+++ index.js
@@ -2,17 +2,24 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
-
 const PORT = 5000;
 
+// Middleware
 app.use(express.json());
 
+// Static Files
 app.use(express.static(path.join(__dirname, "public")));
 
+// Routes
 const taskRoutes = require("./routes/taskRoutes");
-
 app.use("/api/tasks", taskRoutes);
 
+// Home Route
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+// Start Server
 app.listen(PORT, () => {
-    console.log(`🚀 Server running on http://localhost:${PORT}`);
+    console.log(`🚀 Server Running at http://localhost:${PORT}`);
 });
